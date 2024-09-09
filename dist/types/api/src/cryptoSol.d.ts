ebb502c85fd8da1c055d62ac6c5a152b659c4c31
MSalman6 <salman666online@gmail.com>
2024-09-09 12:46:48 +0500

    updated for UI

--- dist/types/api/src/cryptoSol.d.ts
+++ dist/types/api/src/cryptoSol.d.ts
@@ -1,7 +1,5 @@
 import { ClaimContract } from '../../typechain-types/index';
 import { CryptoJS } from './cryptoJS';
-import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
-import { BalanceV3 } from "../data/interfaces";
 /**
  * Crypto functions used in this project implemented in Soldity.
  */
@@ -9,7 +7,6 @@ export declare class CryptoSol {
     instance: ClaimContract;
     cryptoJS: CryptoJS;
     private logDebug;
-    static fromContractAddress(contractAddress: string): Promise<CryptoSol>;
     constructor(instance: ClaimContract);
     claim(dmdV3Address: string, dmdV4Address: string, signature: string, postfix: string): Promise<import("ethers").ContractTransactionReceipt | null>;
     recoverV(dmdV4Address: string, postfixHex: string, pubKeyX: string, pubKeyY: string, r: Buffer, s: Buffer): Promise<string>;
@@ -19,6 +16,4 @@ export declare class CryptoSol {
     pubKeyToEthAddress(x: string, y: string): Promise<string>;
     prefixString(): Promise<string>;
     getBalance(dmdV3Address: string): Promise<bigint>;
-    getContractBalance(): Promise<bigint>;
-    fillBalances(sponsor: SignerWithAddress, balances: BalanceV3[]): Promise<bigint>;
 }
